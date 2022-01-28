c54e867188c279f784a16c57c1f3a5f25c1256f2
Otso Helos <17408120+otsohelos@users.noreply.github.com>
2022-01-28 13:42:06 +0200

    backend fetches one book

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
+const axios = require('axios')
+const bookRouter = require('express').Router()
+
 const http = require('http')
+const baseUrl = "https://openlibrary.org/books/OL7353617M.json"
+
 
 const app = http.createServer((request, response) => {
+    axios.get(`${baseUrl}`).then(res => console.log(res.data))
     response.writeHead(200, { 'Content-Type': 'text/plain' })
     response.end('Hello World')
 })
+
+commentsRouter.get('/', async (request, response) => {
+    const books = await axios.get(`${baseUrl}`)
+    response.json(comments.map(comm => comm.toJSON()))
+  })
 
 const PORT = 3001
 app.listen(PORT)
